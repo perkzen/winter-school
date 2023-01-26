4e71eea3aa14aafec47abe4d9b902927713df972
perkzen <perko.domen@gmail.com>
2023-01-26 09:51:27 +0100

    fix: mongo

--- inventory-management/src/main/java/si/um/feri/inventorymanagement/MongoConfig.java
+++ inventory-management/src/main/java/si/um/feri/inventorymanagement/MongoConfig.java
@@ -15,23 +15,19 @@ public class MongoConfig {
 
     private static final Logger log = Logger.getLogger(MongoConfig.class.toString());
 
-    @Value("${spring.data.mongodb.host}")
-    private String host;
-
-    @Value("${spring.data.mongodb.port}")
-    private String port;
-
     @Value("${spring.data.mongodb.database}")
     private String database;
 
+    @Value("${spring.data.mongodb.uri}")
+    private String uri;
+
     @Bean
     public MongoClient mongo() {
-        log.info(() -> "Connecting to MongoDB: "+"mongodb://"+host+":"+port+"/"+database);
-        ConnectionString connectionString = new ConnectionString("mongodb://"+host+":"+port+"/"+database);
+        log.info(() -> "Connecting to MongoDB: "+ uri);
+        ConnectionString connectionString = new ConnectionString(uri);
         MongoClientSettings mongoClientSettings = MongoClientSettings.builder().applyConnectionString(connectionString).build();
         return MongoClients.create(mongoClientSettings);
     }
-
     @Bean
     public MongoTemplate mongoTemplate() {
         return new MongoTemplate(mongo(), database);
